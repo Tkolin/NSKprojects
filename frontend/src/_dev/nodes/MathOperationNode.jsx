ccf53279d8515ba8646d90fd10047d4e3a66a065
Tkolin <2329192@mail.ru>
2024-04-28 03:38:37 +0700

    DevBuild 0.4.5-5 // Полная начальная интеграция react flow

---
+++ frontend/src/_dev/nodes/MathOperationNode.jsx
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import {Handle} from "reactflow";
+import {tw} from "twind";
+import {useStore} from "../store";
+
+const comput = (id) => (store) => ({
+    setValue: (total) => store.updateNode(id,  {values:{'output': total}} ),
+});
+
+export default function MathOperationNode({id, data}) {
+    const {setValue} = useStore(comput(id));
+
+    const [operation, setOperation] = useState(data?.operation || "+");
+    const handleOperationChange = (e) => {
+        setOperation(e.target.value);
+        data.operation = e.target.value;
+    };
+    if (data.inputs.x && data.inputs.y) {
+        let total = 0;
+        if (data?.operation === "+") {
+            total = data.inputs.x + data.inputs.y;
+        } else if (data?.operation === "-") {
+            total = data.inputs.x - data.inputs.y;
+        } else if (data?.operation === "*") {
+            total = data.inputs.x * data.inputs.y;
+        } else if (data?.operation === "/") {
+            total = data.inputs.x / data.inputs.y;
+        }
+
+        if (data.total  !== total) {
+            data.total = total
+            setValue(total);
+        }
+
+    }
+    return (
+        <div className={tw("rounded-md bg-white shadow-xl")}>
+            <Handle id='x' className={tw("w-2 h-2")} type="target" position="left"/>
+            <Handle id='y' className={tw("w-2 h-2")} type="target" position="right"/>
+
+            <p
+                className={tw("rounded-t-md px-2 py-1 bg-blue-500 text-white text-sm")}
+            >
+                Operation
+            </p>
+            <label className={tw("flex flex-col px-2 pt-1 pb-4")}>
+                <select
+                    className={tw("px-2 py-1")}
+                    value={operation}
+                    onChange={handleOperationChange}
+                >
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="*">*</option>
+                    <option value="/">/</option>
+                </select>
+            </label>
+
+
+            <Handle id='output' className={tw("w-2 h-2")} type="source" position="bottom"/>
+        </div>
+    );
+}
---
+++ frontend/src/_dev/store.js
@@ -0,0 +1,162 @@
+import {applyNodeChanges, applyEdgeChanges} from "reactflow";
+import {nanoid} from "nanoid";
+import {create} from "zustand";
+
+
+export const useStore = create((set, get) => ({
+    nodes: [
+        {
+            id: "inputNode",
+            type: "inputNode",
+            data: {values: {'one': 1}},
+            position: {x: -100, y: 0},
+        },
+        {
+            id: "inputNode1",
+            type: "inputNode",
+            data: {values: {'one': 2}},
+            position: {x: 100, y: 0},
+        },
+        {
+            id: "mathOperationNodeId",
+            type: "mathOperationNode",
+            data: {
+                operation: "+",
+                inputs: {x: 1, y: 2},
+            },
+            position: {x: 50, y: 150},
+        },
+        {
+            id: "mathOperationNodeId1",
+            type: "mathOperationNode",
+            data: {
+                operation: "+",
+                inputs: {x: 1, y: 2},
+            },
+            position: {x: 50, y: 300},
+        }
+    ],
+    edges: [
+        {
+            id: "inputNode->mathOperationNodeId",
+            source: "inputNode",
+            sourceHandle: 'output',
+            target: "mathOperationNodeId",
+            targetHandle: 'x',
+        },
+        {
+            id: "inputNode1->mathOperationNodeId",
+            source: 'inputNode1',
+            sourceHandle: 'output',
+            target: 'mathOperationNodeId',
+            targetHandle: 'y'
+        },
+
+    ],
+
+    onNodesChange(changes) {
+        set({
+            nodes: applyNodeChanges(changes, get().nodes),
+        });
+    },
+
+
+    createNode(type, x, y) {
+
+        const id = nanoid();
+
+        switch (type) {
+            case "inputNode": {
+                const data = {value: 0};
+                const position = {x: 0, y: 0};
+
+                set({nodes: [...get().nodes, {id, type, data, position}]});
+
+                break;
+            }
+
+            case "mathOperationNode": {
+                const data = {gain: '+'};
+                const position = {x: 0, y: 0};
+
+                set({nodes: [...get().nodes, {id, type, data, position}]});
+
+                break;
+            }
+        }
+    },
+    updateNode(id, data) {
+        console.error('updateNode ', id, data)
+        set((state) => {
+            // + Обновляем data в переданном узле
+            const updatedNodes = state.nodes.map((node) =>
+                node.id === id ? {...node, data: {...node.data, ...data}} : node
+            );
+
+            // Получаем список дочерних элементов ОТ этого узла (1 уровень)
+            const connectedNodeIds = state.edges
+                .filter(edge => edge.source === id)
+                .map(edge => edge.target);
+
+            // Обновляем дочерние элементы
+            connectedNodeIds.forEach(connectedNodeId => {
+                const connectedNode = updatedNodes.find(node => node.id === connectedNodeId);
+                //console.error('connectedNode ', connectedNode);
+                if (connectedNode) {
+                    const targetHandle = state.edges.find(edge => edge.target === connectedNodeId && edge.source === id).targetHandle;
+                    // Находим значение выхода текущего узла, которое будем передавать во вход подключенного узла
+                    const sourceHandle = state.edges.find(edge => edge.target === connectedNodeId && edge.source === id).sourceHandle;
+
+                    if (targetHandle && sourceHandle) {
+                        // Добавляем новую пару ключ-значение в объект values у узла mathOperationNode
+                        const updatedInput = {
+                            ...connectedNode.data.inputs,
+                            [targetHandle]: data.values[sourceHandle]
+                        };
+                        const updatedMathNode = {
+                            ...connectedNode,
+                            data: {
+                                ...connectedNode.data,
+                                inputs: updatedInput,
+                            },
+                        };
+                        // Заменяем узел в массиве updatedNodes
+                        updatedNodes.splice(
+                            updatedNodes.findIndex(node => node.id === connectedNodeId),
+                            1,
+                            updatedMathNode
+                        );
+                    }
+                }
+            });
+            console.log('updatedNodes Final', updatedNodes );
+            return {nodes: updatedNodes};
+        });
+    },
+    onNodesDelete(deleted) {
+        const newNodes = get().nodes.filter((node) => !deleted.some((d) => d.id === node.id));
+        set({nodes: newNodes});
+    },
+
+    onEdgesChange(changes) {
+        console.log('onEdgesChange');
+
+        set({
+            edges: applyEdgeChanges(changes, get().edges),
+        });
+    },
+
+    addEdge(data) {
+        console.log('addEdge', data);
+
+        const id = nanoid(6);
+        const edge = {id, ...data};
+
+        set({edges: [edge, ...get().edges]});
+    },
+
+    onEdgesDelete(deleted) {
+        const newEdges = get().edges.filter((edge) => !deleted.some((d) => d.source === edge.source && d.target === edge.target));
+        set({edges: newEdges});
+    },
+}));
