201b393c57c75dcbf63c31c27a585f009f4365fb
Tkolin <2329192@mail.ru>
2024-12-19 11:30:20 +0700

    hotfix

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\FileController;
 use App\Http\Controllers\FileUpload\ProjectExecutor;
 use App\Http\Controllers\FileUpload\ProjectMain;
 use App\Http\Controllers\FileUpload\ProjectStage;
-use App\Http\Controllers\FileUploadController;
+// use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
